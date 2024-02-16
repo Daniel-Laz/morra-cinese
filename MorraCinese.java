348100439b9bb1ffd2353ff1cea3c2eaf09269c5
Daniel-Laz <160122188+Daniel-Laz@users.noreply.github.com>
2024-02-16 13:48:34 +0100

    Rename Main.java to MorraCinese.java

--- MorraCinese.java
+++ MorraCinese.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class MorraCinese {
     public static void main(String[] args) {
         Scanner in=new Scanner(System.in);
         boolean corretto1=false;
